2ff17c76a742ab12fcd3f611218c1927fad5171f
fibre-ether <fire.breather@rediff.com>
2022-10-05 00:30:45 +0530

    remove warnings from main helper

--- src/helpers/main.js
+++ src/helpers/main.js
@@ -1,7 +1,5 @@
 const isAuthenticated = () => {
   const authToken = localStorage.getItem("token");
-  const userType = localStorage.getItem("type");
-  const user = localStorage.getItem("user");
 
   const isAuth = !!authToken;
 
