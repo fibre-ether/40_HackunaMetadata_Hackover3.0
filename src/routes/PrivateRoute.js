1bb6edc579ab76e0b5e221408f81141e8adcb9d6
PrachetShah <prachetshah25@gmail.com>
2022-10-04 19:21:49 +0530

    removed code

--- src/routes/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { isAuthenticated } from "../helpers/main";
-
-export const PrivateRoute = () => {
-  const { isAuth } = isAuthenticated();
-  return isAuth ? <Outlet /> : <Navigate to="/" />;
-};
