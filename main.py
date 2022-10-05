64fbb17b5ae67e75ef6da14d98496eb085c03f8e
PrachetShah <prachetshah25@gmail.com>
2022-10-05 07:08:38 +0530

    some changes

--- main.py
+++ main.py
@@ -30,54 +30,26 @@ def main():
         image_file = 'credit score.png'
         
         img_url = 'https://raw.githubusercontent.com/fibre-ether/40_HackunaMetadata_Hackover3.0/login/src/assets/test_image.jpg'
+        img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Slovenian_ID_Card_2022_-_Front.jpg/250px-Slovenian_ID_Card_2022_-_Front.jpg'
         response = requests.get(img_url)
         if response.status_code:
             fp = open(f'greenland_01a.png', 'wb')
             fp.write(response.content)
             fp.close()
-        GetTextRead('greenland_01a.png')
-        GetTextRead(image_file)
-            
-                
+        
+        output = GetTextRead('greenland_01a.png')
+        # GetTextRead(image_file)  
+        print(output)     
 
     except Exception as ex:
         print(ex)
 
-def GetTextOcr(image_file):
-    print('Reading text in {}\n'.format(image_file))
-    # Use OCR API to read text in image
-    with open(image_file, mode="rb") as image_data:
-        ocr_results = cv_client.recognize_printed_text_in_stream(image_data)
-
-    # Prepare image for drawing
-    fig = plt.figure(figsize=(7, 7))
-    img = Image.open(image_file)
-    draw = ImageDraw.Draw(img)
-
-    # Process the text line by line
-    for region in ocr_results.regions:
-        for line in region.lines:
-
-            # Show the position of the line of text
-            l,t,w,h = list(map(int, line.bounding_box.split(',')))
-            draw.rectangle(((l,t), (l+w, t+h)), outline='magenta', width=5)
-
-            # Read the words in the line of text
-            line_text = ''
-            for word in line.words:
-                line_text += word.text + ' '
-            print(line_text.rstrip())
-
-    # Save the image with the text locations highlighted
-    plt.axis('off')
-    plt.imshow(img)
-    outputfile = f'{image_file}_output.jpg'
-    fig.savefig(outputfile)
-    print('Results saved in', outputfile)
 
 def GetTextRead(image_file):
     print('Reading text in {}\n'.format(image_file))
     # Use Read API to read text in image
+    output = {}
+    output['name'] = image_file
     with open(image_file, mode="rb") as image_data:
         read_op = cv_client.read_in_stream(image_data, raw=True)
 
@@ -99,10 +71,11 @@ def GetTextRead(image_file):
                 for line in page.lines:
                     text.append(str(line.text))
                     print(line.text)
-                with open('note.txt', 'a') as note:
-                    note.write(str(text))
-                    note.write('\n')
-
+                output['data'] = text
+                # with open('note.txt', 'a') as note:
+                #     note.write(str(text))
+                #     note.write('\n')
+    return output
 
 if __name__ == "__main__":
     main()
